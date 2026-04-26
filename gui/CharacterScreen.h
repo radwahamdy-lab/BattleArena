53d549b0b9cf419d27a63bfd401ff535bead5655
Zad <zad.elhosainy@aucegypt.edu>
2026-04-26 13:36:10 +0000

    Added character selection option

--- gui/CharacterScreen.h
+++ gui/CharacterScreen.h
@@ -3,20 +3,66 @@
 #include <QPushButton>
 #include <QStackedWidget>
 #include <QObject>
+#include <QLabel>
+#include <QString>
+#include <cstdlib>
+#include <ctime>
 
 class CharacterScreen {
 	private:
 		QWidget *characterPage = new QWidget;
+
+		QString userCharacter;
+                QString computerCharacter;
+
+	         QString getRandomComputerCharacter() {
+		        int choice = rand() % 3 + 1;
+
+      		        if (choice == 1)
+           	       		 return "Warrior";
+       		        else if (choice == 2)
+          	       		 return "Archer";
+       		        else
+          		         return "Mage";
+    }
+
+		void selectCharacter(QString selectedCharacter) {
+       	        userCharacter = selectedCharacter;
+                computerCharacter = getRandomComputerCharacter();
+    }
+
 	public:
 		CharacterScreen(QStackedWidget* stackedwid){
-			QVBoxLayout *startLayout = new QVBoxLayout(characterPage);
+			srand(time(0));  
 
-			QPushButton *returnBtn = new QPushButton("Return");
+			QVBoxLayout *startLayout = new QVBoxLayout(characterPage);
+			QLabel *title = new QLabel("Choose Your Character");
+			startLayout->addWidget(title);
+
+		        QPushButton *warriorBtn = new QPushButton("Warrior");
+       		        QPushButton *archerBtn = new QPushButton("Archer");
+		        QPushButton *mageBtn = new QPushButton("Mage");
+
+        		QPushButton *returnBtn = new QPushButton("Return");
 			returnBtn->setGeometry(90, 190, 611, 51);
 			returnBtn->setStyleSheet("font: 16pt 'JetBrainsMono Nerd Font Propo'; color: white; background-color: rgb(47, 47, 47);");
 
+			startLayout->addWidget(warriorBtn);
+		        startLayout->addWidget(archerBtn);
+        		startLayout->addWidget(mageBtn);
 			startLayout->addWidget(returnBtn);
 
+        		QObject::connect(warriorBtn, &QPushButton::clicked, [this]() {
+			    selectCharacter("Warrior");
+			});
+
+			QObject::connect(archerBtn, &QPushButton::clicked, [this]() {
+			    selectCharacter("Archer");
+			});
+
+			QObject::connect(mageBtn, &QPushButton::clicked, [this]() {
+			    selectCharacter("Mage");
+			});
 			QObject::connect(returnBtn, &QPushButton::clicked, [stackedwid](){
 				stackedwid->setCurrentIndex(0);
 			});
@@ -25,4 +71,12 @@ class CharacterScreen {
 		QWidget* getPage(){
 			return characterPage;
 		}
+
+		 QString getUserCharacter() {
+      		  return userCharacter;
+    }
+
+ 		 QString getComputerCharacter() {
+	          return computerCharacter;
+    }
 };
