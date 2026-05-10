26a1595ff310a0d3e9771aa5770303cb521d79dd
radwahamdy-lab <radwahamdy@aucegypt.edu>
2026-05-10 12:29:31 +0300

    merge gamescreen.h

--- gui/GameScreen.h
+++ gui/GameScreen.h
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #include <QWidget>
 #include <QVBoxLayout>
 #include <QPushButton>
@@ -20,33 +20,3 @@ class GameScreen {
 		QWidget* getPage();
 
 };
-=======
-#ifndef GAMESCREEN_H
-#define GAMESCREEN_H
-
-#include <QWidget>
-#include <QVBoxLayout>
-#include <QPushButton>
-#include <QStackedWidget>
-#include <QObject>
-#include <QTimer>
-#include "Character.h"
-#include <QGraphicsTextItem>
-#include <QString>
-
-class GameScreen {
-	private:
-		QWidget *gamePage = new QWidget;
-		int time = 180;
-		Character *player;
-		Character *comp;
-        QGraphicsTextItem* timerText;
-	public:
-		GameScreen(QStackedWidget*, int, int);
-		QWidget* getPage();
-        void updateTimer(QString text);
-
-};
-
-#endif
->>>>>>> e6d812dcbce226c3e20c6aa14237d448526b710d
