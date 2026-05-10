14da3b20be0c123ddce2807d83393671a87bb12c
radwahamdy-lab <radwahamdy@aucegypt.edu>
2026-05-10 12:26:37 +0300

    merge chracter.h

--- gui/Character.h
+++ gui/Character.h
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #pragma once
 #include <QGraphicsPixmapItem>
 #include <QKeyEvent>
@@ -39,50 +39,4 @@ class Character : public QGraphicsPixmapItem {
         int score = 0;
 
 };
-=======
-#pragma once
-#include <QGraphicsPixmapItem>
-#include <QKeyEvent>
-#include <QPixmap>
 
-enum CharacterType {
-    ARCHER = 1,
-    WARRIOR = 2,
-    MAGE = 3
-};
-
-class Character : public QGraphicsPixmapItem {
-    public:
-        Character(QGraphicsScene*, int, bool);
-        void setScore(int s);
-        int getScore();
-        int getCharacter();
-        int getDirection();
-        QPixmap* getPixmaps();
-        void setEnemy(QGraphicsItem*);
-    protected:
-        void keyPressEvent(QKeyEvent *event);
-        void keyReleaseEvent(QKeyEvent *event);
-
-    private:
-        QPixmap archer_idle = QPixmap(":characters/Archer/Archer_Idle.png");
-        QPixmap archer_run = QPixmap(":characters/Archer/Archer_Run.png");
-        QPixmap warrior_idle = QPixmap(":characters/Warrior/Warrior_Idle.png");
-        QPixmap warrior_run = QPixmap(":characters/Warrior/Warrior_Run.png");
-        QPixmap warrior_attack1 = QPixmap(":characters/Warrior/Warrior_Attack.png");
-        QPixmap warrior_attack2 = QPixmap(":characters/Warrior/Warrior_Attack2.png");
-        QPixmap mage_idle = QPixmap(":characters/Mage/Mage_Idle.png");
-        QPixmap mage_run = QPixmap(":characters/Mage/Mage_Run.png");
-        QPixmap archer[2] = {archer_idle, archer_run};
-        QPixmap warrior[4] = {warrior_idle, warrior_run, warrior_attack1, warrior_attack2};
-        QPixmap mage[2] = {mage_idle, mage_run};
-        QPixmap* char_ptr;
-        QGraphicsScene* scene;
-        QGraphicsItem* enemy;
-        int speed = 5;
-        int character;
-        int direction;
-        int score = 0;
-
-};
->>>>>>> e6d812dcbce226c3e20c6aa14237d448526b710d
